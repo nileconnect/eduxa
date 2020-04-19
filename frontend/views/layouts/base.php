896b055cbe2cc73178ad82dc616e5cd98e6c9bc0
M Amer <mohamed.amer2050@gmail.com>
2020-04-19 23:10:30 +0200

    fix bootstarb

--- frontend/views/layouts/base.php
+++ frontend/views/layouts/base.php
@@ -45,32 +45,10 @@ $this->beginContent('@frontend/views/layouts/_clear.php')
         </div>
         <div class="header-nav">
             <div class="container">
-
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <div class="navbar-brand logo"><h1><a href="/"><img src="img/logo.svg" alt="eduxa"></a></h1></div>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Universities <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Language Schools</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="button button-primary" href="/login">My Eduxa</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="button button-primary" href="#">Referral Program</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+               <?php
+                       $this->beginContent('@frontend/views/layouts/_menu.php');
+                       $this->endContent() ;
+                   ?>
             </div>
         </div>
     </header>
