703d5c0937ad22dd75c4b6e9606e97d2b038c7a3
amer <amer@gmail.com>
2020-05-30 19:11:27 +0200

    fix

--- frontend/views/universities/view.php
+++ frontend/views/universities/view.php
@@ -164,7 +164,7 @@
                                         ?>
                                         <div class="col-sm-6 mbsm">
                                             <div class="embed-responsive embed-responsive-16by9">
-                                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $universityVideo->base_url?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= MyYoutubeVideoID($universityVideo->base_url); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                                     
                                             </div>
                                         </div>
