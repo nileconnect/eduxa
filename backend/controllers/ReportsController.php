0f6e9fccdc1345779e8046b4723b2ee169529077
MohamedAaa <m.3laa.95@gmail.com>
2020-06-26 13:15:26 +0200

    formate

--- backend/controllers/ReportsController.php
+++ backend/controllers/ReportsController.php
@@ -2,39 +2,30 @@
 
 namespace backend\controllers;
 
-use backend\models\search\RequestsSearch;
-use common\models\User;
-use common\models\UserProfile;
-use Yii;
-use common\models\City;
-use backend\models\CompetitionPath;
-use common\models\District;
-use backend\models\Govenment;
 use backend\models\School;
-use backend\models\SchoolsCompetition;
-use common\models\lookup\Country;
-use yii\db\Query;
-use yii\helpers\Json;
-use yii\web\Response;
+use backend\models\search\RequestsSearch;
+use Yii;
 
 /**
  * SchoolsController implements the CRUD actions for Schools model.
  */
-class ReportsController extends   BackendController
+class ReportsController extends BackendController
 {
 
-    public function actionGeneral(){
+    public function actionGeneral()
+    {
 
-       return  $this->render('general');
+        return $this->render('general');
     }
 
-    public function actionUsers(){
+    public function actionUsers()
+    {
 
-        return  $this->render('users');
+        return $this->render('users');
     }
 
-
-    public function actionRequests(){
+    public function actionRequests()
+    {
 
         $searchModel = new RequestsSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
