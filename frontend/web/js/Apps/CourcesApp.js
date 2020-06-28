399b86b707df1f99744c5f95df0c349449e31696
Mohamed3akl <mohamed3akl@hotmail.com>
2020-06-28 23:38:27 +0200

    fix

--- frontend/web/js/Apps/CourcesApp.js
+++ frontend/web/js/Apps/CourcesApp.js
@@ -331,7 +331,7 @@ var app = new Vue({
                         'students': this.StudentsList,
                         'course_start_date': this.SelectedDate,
                         'accomodation_option': this.selectedaccoID,
-                        'accomodation_period': this.selectedCourseDuration,
+                        'accomodation_period': this.Selectedperiod,
                         'number_of_weeks': this.selectedCourseDuration,
                         'airport_pickup': this.selectedAirportID,
                         'health_insurance': this.selectedHealthIns
@@ -367,7 +367,7 @@ var app = new Vue({
                     // 'students': this.StudentsList,
                     'course_start_date': this.SelectedDate,
                     'accomodation_option': this.selectedaccoID,
-                    'accomodation_period': this.selectedCourseDuration,
+                    'accomodation_period': this.Selectedperiod,
                     'number_of_weeks': this.selectedCourseDuration,
                     'airport_pickup': this.selectedAirportID,
                     'health_insurance': this.selectedHealthIns
