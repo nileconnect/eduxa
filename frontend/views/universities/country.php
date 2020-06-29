87268e079ce289089b975240734ca048dad9ee6b
MohamedAaa <m.3laa.95@gmail.com>
2020-06-29 23:24:33 +0200

    Universities - Top destination - Country : Top destination not exist in country page

--- frontend/views/universities/country.php
+++ frontend/views/universities/country.php
@@ -3,14 +3,42 @@
         <div class="img-layer" style="background-image: url('/img/banners/banner1.jpg');"></div>
     </div>
 
-    <div class="search-form after-jumbotron">
-    <div class="container">
-        <div class="text-white">
-            <h2>Are you interested in studying in <?= $countryObj->title ?>?</h2>
-            <h5>Find, Review and Apply to the best universities in the world</h5>
+
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?php
+    if($countries){
+    ?>
+    <section class="section top-destinations">
+        <div class="container">
+            <h1 class="title text-center"><?= Yii::t('frontend','Top Destinations') ?></h1>
+
+            <div class="mtlg">
+                <ul data-slider="slick" data-slick='{"slidesToShow": 4, "slidesToScroll": 1, "responsive": [{"breakpoint": 768, "settings": { "slidesToShow": 1 }}, {"breakpoint": 480,"settings": {"slidesToShow": 1} }]}'>
+                <?php
+                foreach ($countries as $country) {
+                    ?>
+
+                    <li>
+                        <figure>
+                            <a href="/universities/country/<?= $country->code?>">
+                                <img src="<?= $country->flag ?>" alt="<?= $country->title ?>" width="270px" height="270px" />
+                                <figcaption>
+                                    <span class="country-name"><?= $country->title ?></span>
+                                </figcaption>
+                            </a>
+                        </figure>
+                    </li>
+                    <?
+                }
+                ?>
+
+                </ul>
+            </div>
         </div>
-    </div>
-    </div>
+    </section>
+    <?php } ?>
+
     <div class="clear"><br/><br/></div>
     <section class="section">
         <div class="container">
