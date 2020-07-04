4126bdfc3fe10f0d69da8846c01e419bf07e7d55
Mohamed3akl <mohamed3akl@hotmail.com>
2020-07-04 19:41:22 +0200

    fix

--- frontend/views/page/view.php
+++ frontend/views/page/view.php
@@ -5,18 +5,28 @@
  */
 $this->title = $model->title;
 ?>
+<nav aria-label="breadcrumb">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $model->title ?></li>
+        </ol>
+    </div>
+</nav>
 
 
-
-<section class="section">
+<!-- <section class="section">
     <div class="container">
         <div class="row mbxlg">
             <div class="col-sm-12 mbmd">
                 <h3 class="title title-sm"><?php echo $model->title ?></h3>
             </div>
             <div class="col-sm-12 mbmd">
-                <?php echo $model->body ?>
+                
             </div>
         </div>
     </div>
-</section>
+</section> -->
+
+
+<?php echo $model->body ?>
