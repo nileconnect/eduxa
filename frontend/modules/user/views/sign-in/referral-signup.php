2ae33970747737635be7bc1c581cdd7b6967a1b6
Mohamed3akl <mohamed3akl@hotmail.com>
2020-07-21 21:38:28 +0200

    fix

--- frontend/modules/user/views/sign-in/referral-signup.php
+++ frontend/modules/user/views/sign-in/referral-signup.php
@@ -176,9 +176,9 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 <div class="successMsg <?php if(Yii::$app->session->hasFlash('alert-create-account-successfully')) echo 'show'; ?>">
     <img src="/img/success.png">
-    <h3>Congratulations,</h3>
-    <p>Your Account Successfully Created, Please check your email inbox to activate your account.</p>
-        <a class="button button-primary" href="/login">Home</a>
+    <h3><?= Yii::t('frontend', 'Congratulations'); ?>,</h3>
+    <p><?= Yii::t('frontend', 'Your Account Successfully Created, Please check your email inbox to activate your account.'); ?></p>
+        <a class="button button-primary" href="/login"><?= Yii::t('frontend', 'Home'); ?></a>
 </div>
 
 <?php
