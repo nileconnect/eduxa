283132df032d2e3bf99a83a4553a01849c2ec8d8
amer <amer@gmail.com>
2020-07-24 15:12:36 +0200

    fix

--- frontend/web/js/Apps/Api.js
+++ frontend/web/js/Apps/Api.js
@@ -1,7 +1,7 @@
 if (window.location.href.indexOf("localhost") > -1) {
     var Api = "http://eduxa.localhost/rest/";
-} else if (window.location.href.indexOf("ahmedabdelaziz") > -1) {
-    var Api = "http://ahmedabdelaziz.net/rest/";
+} else if (window.location.href.indexOf("eduxa") > -1) {
+    var Api = "https://eduxa.com/rest/";
 } else {
     var Api = "http://eduxa.localhost/rest/";
 }
