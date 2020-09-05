724b94d0046349d136c7340ccf724673501ca1d3
MohamedAaa <m.3laa.95@gmail.com>
2020-09-05 06:31:11 +0200

    trans

--- frontend/modules/user/views/sign-in/signup.php
+++ frontend/modules/user/views/sign-in/signup.php
@@ -175,9 +175,9 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 </div>
-<div class="successMsg <?php if(Yii::$app->session->hasFlash('alert-create-user-account-successfully')) echo 'show'; ?>">
+<div class="successMsg <?php if(Yii::$app->session->hasFlash('alert-create-account-referral-successfully')) echo 'show'; ?>">
     <img src="/img/success.png">
-    <h3>Congratulations,</h3>
-    <p>Your Account Successfully Created, Please check your email inbox to activate your account.</p>
-        <a class="button button-primary" href="/login">Home</a>
+    <h3><?=  Yii::t('frontend','Congratulations') ?>,</h3>
+    <p><?= Yii::t('frontend','Your Account Successfully Created, and we will be verified your account by our team.')?></p>
+    <a class="button button-primary" href="/login"><?= Yii::t('frontend','Home')?></a>
 </div>
