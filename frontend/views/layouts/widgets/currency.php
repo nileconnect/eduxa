38b24e90190ad02371edd1821d1376047b777522
amer <amer@gmail.com>
2020-10-03 00:51:17 +0200

    fix

--- frontend/views/layouts/widgets/currency.php
+++ frontend/views/layouts/widgets/currency.php
@@ -1,5 +1,8 @@
 <?php
-$currencies= \backend\models\Currency::findAll(['status'=>1]);
+$currencies= \backend\models\ Currency::findAll(['status'=>1]);
+
+$currencies = ['DZD','BHD','EGP','EUR','IQD','JOD','KWD','LYD','MAD','OMR','QAR','SAR','SDG','SYP','TND','AED','GBP','USD','YER'];
+
 use common\helpers\MyCurrencySwitcher;
 ?>
 
@@ -8,7 +11,8 @@ use common\helpers\MyCurrencySwitcher;
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
         <?php
         foreach ($currencies as $currency) {
-            echo '<a class="dropdown-item" href="'.MyCurrencySwitcher::createMulticurrencyReturnUrl($currency->currency_code).'">'.$currency->currency_code.'</a>';
+            //echo '<a class="dropdown-item" href="'.MyCurrencySwitcher::createMulticurrencyReturnUrl($currency->currency_code).'">'.$currency->currency_code.'</a>';
+            echo '<a class="dropdown-item" href="'.MyCurrencySwitcher::createMulticurrencyReturnUrl($currency).'">'.$currency.'</a>';
         }
         ?>
     </div>
